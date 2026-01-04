bd97bedf8eb88813664b69f14d610da927c1a8a2
Eli <elijah2307.williams@gmail.com>
2026-01-04 15:42:44 +0000

    library converter code cleanup and adding remove leftover files mode where files in compressed library that don't exist in original are removed

--- README.md
+++ README.md
@@ -3,3 +3,11 @@ which allows for transfering music library from computer to phone
 but only transfering new files 
 and will run converter on server side 
 
+
+
+TODO:
+add rust music converter to server 
+add remove leftover files to android app (server and client routes)
+cleanup android app, make buttons more unified and simplier to use 
+
+
--- library_converter/src/main.rs
+++ library_converter/src/main.rs
@@ -1,11 +1,11 @@
 use std::path::Path;
+use std::fs::remove_file;
 use std::fs;
 use std::fs::create_dir_all;
 use std::fs::read_dir;
 use std::process::Command;
 
-fn file_tree(path:&String, sorted:bool, include_hidden:bool)-> Option<Vec<String>>{
-
+fn file_tree(path:&String, sorted:bool, include_hidden:bool)-> Vec<String>{
     let mut vec = Vec::new();
     let mut directories = Vec::new();
     directories.push(Path::new(&path).to_path_buf());
@@ -13,9 +13,9 @@ fn file_tree(path:&String, sorted:bool, include_hidden:bool)-> Option<Vec<String
         let files = read_dir(&directories.remove(0)).unwrap();
         for file in files{
             let f = file.unwrap();
-            let file_path = f.path().to_str()?.to_string();
+            let file_path = f.path().to_str().unwrap().to_string();
             if !include_hidden{
-                if f.file_name().to_str()?.chars().nth(0).unwrap() == '.'{
+                if f.file_name().to_str().unwrap().chars().nth(0).unwrap() == '.'{
                     continue;
                 }
             }
@@ -31,7 +31,7 @@ fn file_tree(path:&String, sorted:bool, include_hidden:bool)-> Option<Vec<String
         vec.sort()
     }
 
-    return Some(vec)
+    return vec
 }
 
 
@@ -61,7 +61,7 @@ fn mp3_to_flac(str:String) -> String{
 }
 //removes prefix from all strings in v
 //doesn't check if elements start with prefix or are long enough 
-fn remove_prefix(v :Vec<String>, prefix:String) -> Vec<String>{
+fn remove_prefix(v :Vec<String>, prefix:&String) -> Vec<String>{
     let mut out = Vec::new();
     for elem in v {
         let converted = elem.chars().skip(prefix.len()).collect();
@@ -96,59 +96,98 @@ fn find_missing(full_vec:Vec<String>,missing_vec:Vec<String>)-> Vec<String>{
 }
 
 fn get_uncompressed_file_list(path:&String) -> Vec<String>{
-    let list = remove_prefix(file_tree(path, false,false).unwrap(),path.to_string());
+    let list = remove_prefix(file_tree(path, false,false),path);
     let mut files: Vec<String> = list.iter().map(|str| clean_flac_file(str.to_string())).collect();
     files.sort();
     return files;
 }
+fn get_compressed_file_list(path:&String) -> Vec<String>{
+    let list = remove_prefix(file_tree(path,true,false),path);
+    return list;
+}
 
-fn  move_and_convert_files(input_path:String, output_path:String, files:Vec<String>){
-    //for file in files
-    //check if parent directory exists in output path 
-    //e.g. files[2] is a/b/c/s and outputPath doesn't have a folder a/b/c/s inside of it 
-    //make sure it exists
-    //then if file  exists in input path then copy it to output path
-    //if doesn't exist then replace suffic with .flac and run ffmpeg to convert to mp3 
-    //inside correct folder
-    for file in files {
-        let full_path_str :String = output_path.clone() + file.as_str();
-        let path = Path::new(full_path_str.as_str()); 
-        let _ = create_dir_all(path.parent().unwrap());
 
-        let mut original_path_str = input_path.clone() + file.as_str();
-        if fs::exists(&original_path_str).unwrap(){
-            let p = Path::new(original_path_str.as_str());
-            let _ = std::fs::copy(p, path);
-        }
-        else{
-            original_path_str= mp3_to_flac(original_path_str);
-            let _original_path = Path::new(original_path_str.as_str());
-            let args = ["-i",&original_path_str,"-acodec","libmp3lame",&full_path_str];
-            //let args = format_args!("-i '{}' -acodec libmp3lame '{}'",original_path,path);
-            let mut output = Command::new("ffmpeg")
-                .args(args).spawn().unwrap() ;
-            let out = output.wait().unwrap();
+fn move_and_convert(input_path:&String, output_path:&String,file:&String, verbose:bool){
+    let full_path_str :String = output_path.clone() + file.as_str();
+    let path = Path::new(full_path_str.as_str()); 
+    let _ = create_dir_all(path.parent().unwrap());
+    let mut original_path_str = input_path.clone() + file.as_str();
+    if fs::exists(&original_path_str).unwrap(){
+        let p = Path::new(original_path_str.as_str());
+        let _ = std::fs::copy(p, path);
+    }
+    else{
+        original_path_str= mp3_to_flac(original_path_str);
+        let _original_path = Path::new(original_path_str.as_str());
+        let args = ["-i",&original_path_str,"-acodec","libmp3lame",&full_path_str];
+        //let args = format_args!("-i '{}' -acodec libmp3lame '{}'",original_path,path);
+        let mut output = Command::new("ffmpeg")
+            .args(args).spawn().unwrap() ;
+        let out = output.wait().unwrap();
+        if verbose{
             println!("{out}");
-
         }
-        
-        
 
+    }   
+}
+
+fn  move_and_convert_files(input_path:&String, output_path:&String, files:Vec<String>, verbose:bool){
+    for i in 0..files.len(){
+        move_and_convert(input_path,output_path,&files[i],verbose);
+        println!("Completed {i} out of {}",files.len());
+        println!("{}%",(i/files.len())*100);
     }
     return;
 }
 
+fn convert_library(input_path:&String,output_path:&String,verbose:bool){
+    let input_files = get_uncompressed_file_list(&input_path);
+    let ouput_files = get_compressed_file_list(&output_path);
+    let missing_files = find_missing(input_files,ouput_files);
+    println!("Number of missing files {}",missing_files.len());
+    move_and_convert_files(input_path, output_path, missing_files,verbose);
+}
+
+fn remove_files(files:Vec<String>, start_path:&String){
+    for file in files{
+        let full_path = start_path.clone() + "/" + &file;
+        let _ = remove_file(full_path);
+        
+    }
+}
+
+fn remove_leftover_files(original_path:&String, compressed_path : &String, verbose:bool){
+    let original_files = get_uncompressed_file_list(original_path);
+    let compressed_files = get_compressed_file_list(compressed_path);
+    let leftover = find_missing(compressed_files, original_files);
+    if verbose {
+        for file in &leftover{
+            println!("{file}");
+        }
+    }
+    println!("Number of leftover files {}",leftover.len());
+    remove_files(leftover, compressed_path);
+}
+
+fn music_converter(uncompressed_path:String, compressed_path:String,mode:String, verbose:bool){
+    if mode == "convert"{
+        convert_library(&uncompressed_path,&compressed_path,verbose);
+    }
+    else if mode == "leftover"{
+
+        remove_leftover_files(&uncompressed_path, &compressed_path, verbose)
+    }
+    else if mode=="both"{
+        convert_library(&uncompressed_path,&compressed_path,verbose);
+        remove_leftover_files(&uncompressed_path, &compressed_path, verbose)
+    }
+}
+
 fn main() {
     let uncompressed_path:String = String::from("/mnt/Data/Music/Library/");
-    let compressed_library_path = String::from("/mnt/Data/mp3Lib/");
-    let compressed_files = remove_prefix(file_tree(&compressed_library_path,true,false).unwrap(), compressed_library_path.clone());
-    let uncompressed_files = get_uncompressed_file_list(&uncompressed_path);
-    
-
-    let missing_files = find_missing(uncompressed_files,compressed_files);
-    println!("Number of missing files {}",missing_files.len());
-    move_and_convert_files(uncompressed_path, compressed_library_path, missing_files)
-    //some files in missing_files e.g. 01.mp3 could be 01.flac or 01.mp3 in the uncompressed files
-    //due to converting them to mp3 to allow for easier checking 
-    //so will need to check both 
+    let compressed_path = String::from("/mnt/Data/mp3Lib/");
+    let verbose = false;
+    let mode = String::from("leftover");
+    music_converter(uncompressed_path, compressed_path,mode, verbose)
 }
+
